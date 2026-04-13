6990e8b2e2adc6a1f808774829893bb25ed483fc
Nightwielder23 <ayandeshpande@hotmail.com>
2026-04-12 22:39:16 -0700

    initial MDK setup

---
+++ src/main/java/com/nightwielder/apothsb/ApothSB.java
@@ -0,0 +1,11 @@
+package com.nightwielder.apothsb;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(ApothSB.MOD_ID)
+public class ApothSB {
+    public static final String MOD_ID = "apothsb";
+
+    public ApothSB() {
+    }
+}
